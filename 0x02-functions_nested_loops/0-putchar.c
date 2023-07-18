2720d53714165f1dc5c3a33c59dd576a97af2765
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-07-18 22:34:15 +0000

    97

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
-	/**
-	 * main - Entry point of code
-	 * is written by Jewel
-	 * Return: 0
-	 */
-	
+/**
+* main - Entry point of code
+* is written by Jewel
+* Return: 0
+*/
+
 	int main(void)
 	{
 	_putchar('_');
