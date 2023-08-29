829931d463d68666ae8b8283ce14affc9bf4f0b2
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-08-29 21:15:02 +0000

    from count to num

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,15 +7,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0; // Initialize a counter for nodes
+    size_t num = 0; // Initialize a counter for nodes
 
     // Traverse the linked list
     while (h)
     {
         printf("%d\n", h->n); // Print the value of the current node
-        count++; // Increment the node count
+        num++; // Increment the node count
         h = h->next; // Move to the next node
     }
 
-    return (count); // Return the number of nodes
+    return (num); // Return the number of nodes
 }
