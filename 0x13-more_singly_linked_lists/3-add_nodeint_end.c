20f1fa5fd87b31c462b9db72c2432f38b28af12d
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-08-30 01:54:00 +0000

    //4add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,8 +5,7 @@
  * @head: pointer to the first element in the list
  * @n: data to insert in the new element
  *
- * Return: pointer to the new node, or NULL if it fails nodeint_end.c
-	if (!new)
+ * Return: pointer to the new node, or NULL if it fails
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
