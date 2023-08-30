17e0025ab1f11ddb0ced61bbf1959739de0e0b68
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-08-30 02:05:46 +0000

    pop_listint.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 
 /**
  * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed free_listint2
+ * @head: pointer to the listint_t list to be freed free_listint2 pop_listint
  */
 void free_listint2(listint_t **head)
 {
