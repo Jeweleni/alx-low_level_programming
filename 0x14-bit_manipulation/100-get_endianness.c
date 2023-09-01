42638a025d703656d1173e1eaeeb884f8fcb9ebe
Eni Momoreoluwa Jewel <109070607+Jeweleni@users.noreply.github.com>
2023-09-01 02:55:45 +0000

    101-password

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
- * get_endianness - Checks the endianness of the system.
- * Return: 0 if big endian, 1 if little endian.
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little 100-get_endianness.c
  */
 int get_endianness(void)
 {
-    unsigned int num = 1;
-    char *byte = (char *)&num;
+	unsigned int i = 1;
+	char *c = (char *) &i;
 
-    if (*byte)
-        return 1;
-    else
-        return 0;
+	return (*c);
 }
